74867c9ba201c6daa2e197c26b92d87cc92bbdeb
s4b3r6 <s4b3r6@gmail.com>
2015-04-30 08:57:45 +1000

    Better Travis install

--- tests/test.py
+++ tests/test.py
@@ -1,18 +1,8 @@
 import subprocess
 
-def buildup():
-     subprocess.call("sudo apt-get install curl -y", shell=True)
-     subprocess.call("sudo apt-get install python -y", shell=True)
-     subprocess.call("sudo apt-get install python-dev -y", shell=True)
-     subprocess.call("sudo apt-get install build-essential -y", shell=True)
-     subprocess.call("sudo apt-get install libffi-dev -y", shell=True)
-     subprocess.call("sudo apt-get install git -y", shell=True)
-     subprocess.call("curl --silent --show-error --retry 5 https://bootstrap.pypa.io/get-pip.py | sudo python", shell=True)
-
 class TestRun(unittest.TestCase):
 
     def test_run(self):
-        buildup()
         """Test to see if it even runs."""
         try:
             subprocess.call("python main.py", shell=True)
