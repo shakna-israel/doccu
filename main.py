bb943860fee0b1a9ea264397cdb9bc3c5aad5c1c
shakna-israel <jmilne@graphic-designer.com>
2015-04-30 07:40:14 +0000

    First parts of REST appearing

--- main.py
+++ main.py
@@ -8,13 +8,30 @@ from gevent import monkey
 # Using gevent, we monkeypatch Python's routing system, so that we end up using greenlets without having to mess with our routes.
 monkey.patch_all()
 
-from bottle import run, CherryPyServer
-import lazyjson
+from bottle import run, CherryPyServer, route
+import json
 
+def doccu_vars():
+    version = "0.0.1"
+    name = "Semantic Arguments"
+    return ( version, name )
+
+@route("/api")
 @route("/api/")
-@route("/api/version")
-def api_version():
-  version_file = lazyjson.File('json/version.json')
-  return version_file
+def api_information():
+    version = doccu_vars()[0]
+    name = doccu_vars()[1]
+    return { 'Version': version, 'Build Name': name }
 
-run(server=CherryPyServer)
+@route("/api/version")
+@route("/api/version/")
+def api_version():
+    version = doccu_vars()[0]
+    return { 'Version': version }
+
+@route("/api/name")
+@route("/api/name/")
+    name = doccu_vars()[1]
+    return { 'Build Name': name }
+
+run(host='0.0.0.0', server=CherryPyServer)
