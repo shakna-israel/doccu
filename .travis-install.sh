a01a880e2e29968af89eb147b9e49138e65b6a88
shakna-israel <jmilne@graphic-designer.com>
2015-05-04 05:16:38 +0000

    Removed broken test structure

--- .travis-install.sh
+++
@@ -1,7 +0,0 @@
-sudo apt-get install curl -y
-sudo apt-get install python -y
-sudo apt-get install python-dev -y
-sudo apt-get install build-essential -y
-sudo apt-get install libffi-dev -y
-sudo apt-get install git -y
-pip install -r requirements.txt
