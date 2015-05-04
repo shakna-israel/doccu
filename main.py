ef87f96ae51e1cb984cb5cd11125f1290fb2ed2b
shakna-israel <jmilne@graphic-designer.com>
2015-05-04 01:41:56 +0000

    Able to fetch document data

--- main.py
+++ main.py
@@ -14,19 +14,37 @@ import uuid
 import base64
 
 def get_latest_version(file):
-    versions = yaml.load(file)
+    openfile = open(file,'r')
+    versions = yaml.load(openfile)
     previous = '0.0.0'
     for key in versions:
         if key["version"] > previous:
             previous = key["version"]
             previous_name = key["name"]
-    file.close()
+    openfile.close()
     return ( previous, previous_name )
+
+def get_document_features(file):
+    openfile = open(file,'r')
+    yaml_data = yaml.load(openfile)
+    version = '0.0.0'
+    for key in yaml_data:
+        if key["version"] > version:
+            version = key["version"]
+            title = key["name"]
+            author = key["author"]
+            signer = key["signer"]
+            group = key["group"]
+            category = key["category"]
+            timestamp = key["timestamp"]
+            body = key["body"]
+    openfile.close()
+    return ( version, title, author, signer, group, category, timestamp, body )            
 
 @route("/api")
 @route("/api/")
 def api_information():
-    file = open("yaml/versions.yaml",'r')
+    file = "yaml/versions.yaml"
     grab = get_latest_version(file)
     version = grab[0]
     name = grab[1]
@@ -35,21 +53,32 @@ def api_information():
 @route("/api/version")
 @route("/api/version/")
 def api_version():
-    version = doccu_vars()[0]
+    file = "yaml/versions.yaml"
+    version = get_latest_version(file)[0]
     return { 'Version': version }
 
 @route("/api/name")
 @route("/api/name/")
 def api_name():
-    name = doccu_vars()[1]
+    file = "yaml/versions.yaml"
+    name = get_latest_version(file)[1]
     return { 'Build Name': name }
     
 @route("/api/document/<name>", method="GET")
 @route("/api/document/<name>/", method="GET")
 def document_fetch(name):
-    # Fetch from JSON file.
-    return { 'Implemented': False }
-
+    openfile = "yaml/" + str(name) + ".yaml"
+    grab_latest = get_document_features(openfile)
+    version = grab_latest[0]
+    title = grab_latest[1]
+    author = grab_latest[2]
+    signer = grab_latest[3]
+    group = grab_latest[4]
+    category = grab_latest[5]
+    timestamp = grab_latest[6]
+    body = grab_latest[7]
+    return { "Version": version, "Title": title, "Author": author, "Signer": signer, "Group": group, "Category": category, "Timestamp": timestamp, "Body": body }
+    
 @route("/api/document/<name>", method="PUT")
 @route("/api/document/<name>/", method="PUT")
 def document_fetch(name):
@@ -66,7 +95,7 @@ def get_uuid():
     return r_uuid.replace('=', '_')
 
 def main():
-    version = get_latest_version(open("yaml/versions.yaml",'r'))
+    version = get_latest_version("yaml/versions.yaml")
     print "Running v" + version[0] + ", " + version[1]
     run(host='0.0.0.0', server=CherryPyServer)
 
