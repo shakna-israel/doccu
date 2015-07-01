424e4a1457c24d8416b2a939ad5f85a355a862de
Shakna Israel <s4b3r6@gmail.com>
2015-07-01 22:11:20 +1000

    Fixed space/tab issue in Management Console.

--- manage.py
+++ manage.py
@@ -6,31 +6,31 @@ from os.path import expanduser
 import os
 
 def generate_id(name,code):
-	db = pickle.load(open("ids.dbs", "rb"))
-	if code in db.values():
-		print("Code not unique!")
-		wait = raw_input("Press enter to exit.")
-		sys.exit()
-	if name in db.keys():
-		print("User already exists!")
-		choice = raw_input("Overwrite? Y/N")
-		if choice == 'y':
-			print("Overwriting...")
-		elif choice == 'Y':
-			print("Overwriting...")
-		else:
-			print("Not overwriting.")
-			wait = raw_input("Press enter to exit.")
-			sys.exit()
-	db[name] = code
-	pickle.dump(db,open("ids.dbs","wb"))
-	print("User written to database!")
+    db = pickle.load(open("ids.dbs", "rb"))
+    if code in db.values():
+        print("Code not unique!")
+        wait = raw_input("Press enter to exit.")
+        sys.exit()
+    if name in db.keys():
+        print("User already exists!")
+        choice = raw_input("Overwrite? Y/N")
+        if choice == 'y':
+            print("Overwriting...")
+        elif choice == 'Y':
+            print("Overwriting...")
+        else:
+            print("Not overwriting.")
+            wait = raw_input("Press enter to exit.")
+            sys.exit()
+    db[name] = code
+    pickle.dump(db,open("ids.dbs","wb"))
+    print("User written to database!")
 
 def remove_id(name):
-	db = pickle.load(open("ids.dbs", "rb"))
-	del db[name]
-	pickle.dump(db,open("ids.dbs","wb"))
-	print("User removed from database!")
+    db = pickle.load(open("ids.dbs", "rb"))
+    del db[name]
+    pickle.dump(db,open("ids.dbs","wb"))
+    print("User removed from database!")
 
 def update_js():
     url_request = requests.get('https://raw.githubusercontent.com/bpampuch/pdfmake/master/build/pdfmake.min.js', stream=True)
@@ -59,44 +59,44 @@ def gen_folder_struct():
         os.makedirs(doccu_docs)
 
 def main():
-        gen_folder_struct()
-	choice = raw_input("Enter 1 to ADD a user, and 2 to REMOVE a User, 3 to START the server, and 4 to updated browser-based dependencies:")
-	if str(choice) == '1':
-		unique_name = raw_input("Enter a users UNIQUE name, e.g. Trevor Clough: ")
-		unique_name = unique_name.strip()
-		unique_name = unique_name.lower()
-		unique_code = raw_input("Enter a users UNIQUE code, e.g. 00226677 ")
-		unique_code = unique_code.strip()
-		generate_id(unique_name,unique_code)
-		print("Done!")
-		wait = raw_input("Press enter to try exit.")
-		sys.exit()
-	elif str(choice) == '2':
-		unique_name = raw_input("Enter the user's UNIQUE name, e.g. Trevor Clough: ")
-		unique_name = unique_name.strip()
-		remove_id(unique_name)
-		print("Done!")
-		wait = raw_input("Press enter to try exit.")
-		sys.exit()
-	elif str(choice) == '3':
-		 try:
-                     subprocess.call(["python","app.py"])
-		 except SystemExit:
-                     try:
-                         subprocess.call(["python","app.py"])
-                     except SystemExit:
-                         subprocess.call(["python","app.py"])
-                     except KeyboardInterrupt:
-                        sys.exit()
-                 except KeyboardInterrupt:
-                        sys.exit()
-	elif str(choice) == '4':
-                 print("Updating javascript dependencies...")
-                 update_js()
-                 print("Updated!")
-        else:
-		choice = None
-		main()
+    gen_folder_struct()
+    choice = raw_input("Enter 1 to ADD a user, and 2 to REMOVE a User, 3 to START the server, and 4 to updated browser-based dependencies:")
+    if str(choice) == '1':
+        unique_name = raw_input("Enter a users UNIQUE name, e.g. Trevor Clough: ")
+        unique_name = unique_name.strip()
+        unique_name = unique_name.lower()
+        unique_code = raw_input("Enter a users UNIQUE code, e.g. 00226677 ")
+        unique_code = unique_code.strip()
+        generate_id(unique_name,unique_code)
+        print("Done!")
+        wait = raw_input("Press enter to try exit.")
+        sys.exit()
+    elif str(choice) == '2':
+        unique_name = raw_input("Enter the user's UNIQUE name, e.g. Trevor Clough: ")
+        unique_name = unique_name.strip()
+        remove_id(unique_name)
+        print("Done!")
+        wait = raw_input("Press enter to try exit.")
+        sys.exit()
+    elif str(choice) == '3':
+        try:
+            subprocess.call(["python","app.py"])
+        except SystemExit:
+            try:
+                subprocess.call(["python","app.py"])
+            except SystemExit:
+                subprocess.call(["python","app.py"])
+            except KeyboardInterrupt:
+                sys.exit()
+        except KeyboardInterrupt:
+            sys.exit()
+    elif str(choice) == '4':
+        print("Updating javascript dependencies...")
+        update_js()
+        print("Updated!")
+    else:
+        choice = None
+        main()
 
 if __name__ == "__main__":
-	main()
+    main()
